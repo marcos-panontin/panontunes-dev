eb20b5d86876b6034e86699728cba3e1442e7d1b
Marcos Panontin <amarcospanontin@gmail.com>
2023-05-16 16:29:37 -0300

    feat(5): implement SearchField

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    // artist: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const minimumLength = 2;
+    const isDisabled = value.length < minimumLength;
+    this.setState({
+      [name]: value,
+      isDisabled,
+    });
+  };
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search-artist">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              id="search-artist"
+              onChange={ this.handleChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ isDisabled }
+            >
+              Pesquisar
+
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
