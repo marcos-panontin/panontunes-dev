fe8e876ed98b4654eb068b5e209cbf9548b87dfc
Marcos Panontin <amarcospanontin@gmail.com>
2023-05-16 16:22:02 -0300

    feat(4): implements navigation Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import LoadingMessage from './LoadingMessage';
 import { getUser } from '../services/userAPI';
 
@@ -28,6 +29,9 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <h1 data-testid="header-user-name">{username}</h1>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
