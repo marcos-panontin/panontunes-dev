9cb9b1a9e76c1bc46bda5b8f509066eb614258a0
Marcos Panontin <amarcospanontin@gmail.com>
2023-05-16 16:01:22 -0300

    feat(2): creates identification form

---
+++ src/components/LoadingMessage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoadingMessage extends React.Component {
+  render() {
+    return (
+      <p>Carregando...</p>
+    );
+  }
+}
+
+export default LoadingMessage;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,77 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import LoadingMessage from '../components/LoadingMessage';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    buttonDisabled: true,
+    isLoading: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const minimumLength = 3;
+    this.setState({
+      [name]: value,
+      buttonDisabled: value.length < minimumLength,
+    });
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+
+    this.setState({
+      isLoading: true,
+    });
+
+    await createUser({ name });
+
+    history.push('/search');
+  };
+
   render() {
-    return (<div data-testid="page-login" />);
+    const { name, buttonDisabled, isLoading } = this.state;
+    if (isLoading) {
+      // return (<LoadingMessage />);
+      return (<p>Carregando...</p>);
+    }
+    return (
+      <div data-testid="page-login">
+        <form>
+
+          <label htmlFor="name-input">
+            Nome:
+            <input
+              name="name"
+              value={ name }
+              type="text"
+              data-testid="login-name-input"
+              id="name-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            data-testid="login-submit-button"
+            disabled={ buttonDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+
+        </form>
+      </div>
+    );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
